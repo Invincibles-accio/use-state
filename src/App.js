9e532085d33ec2efc50f92d94b2d51e84ffb9b0a
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-04-07 22:34:21 +0530

    use state basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+ // count = 0 ,  setCount is a function (inbuilt)
+          // x = > array of two elements
+const App = () => {
+          let [count, setCount]  = useState(0) // 1
+          console.log("I loaded again")  // why it is loading twice
+          let a = 10 
+         function increaseCount(){
+             console.log("I am getting accessed")
+              setCount(count+1) // count = 1
+              // setCount(count+1) // count = 2
+              a = a+1 // 11
+              console.log("inside function ", a) // 11
+         }
+         console.log("outside function",a)
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <h1> {count} </h1>
+         <button onClick={increaseCount}> Increase </button>
     </div>
   );
 }
 
 export default App;
+
+
+
+
